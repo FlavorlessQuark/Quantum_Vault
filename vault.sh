9ffbbc63370fe63a53a2862f8b9b7c1555c4c1a1
OSXVM <osxvm@OSXVMs-Mac.local>
2020-08-21 02:30:52 -0700

    Fixed Dark Matter leakage

--- vault.sh
+++ vault.sh
@@ -8,16 +8,19 @@ while : ; do
 	case "$1" in
 		init)
 			echo -e "\033[38;5;9;1m Initializing repository ...\033[0m"
-			mkdir srcs incl
+			mkdir -p srcs incl
 			touch srcs/main.c
-			echo -e "\033[38;5;46;1m Initializing repository ...\033[0m"
+			mv Quantum_Vault/Makefile .
+			echo -e "\033[38;5;46;1m Initialized ...\033[0m"
 			shift ;;
 		sdl)
 			echo -e "\033[38;5;9;1m Opening wormhole to SDL universe...\033[0m"
 			mkdir -p lib
 			git clone https://github.com/FlavorlessQuark/SDL_Tools tmp
-			mv tmp/*.h ../incl
-			mv tmp/*.a ../lib
+			cp tmp/*.h incl/
+			cp tmp/*.c srcs/
+			cp tmp/*.a lib/
+			rm -rf tmp
 			echo -e "\033[38;5;46;1m Loaded SDL helper\033[0m"
 
 			shift ;;
@@ -25,21 +28,27 @@ while : ; do
 			echo -e "\033[38;5;9;1m Summoning a portal to the Math dimension...\033[0m"
 			mkdir -p lib
 			git clone https://github.com/FlavorlessQuark/Math_Tools tmp
-			mv tmp/*.h ../incl
-			mv tmp/*.a ../lib
+			cp tmp/*.h ../incl
+			cp tmp/*.c ../srcs
+			cp tmp/*.a ../lib
+			rm -rf tmp
+			echo -e "\033[38;5;46;1m Loaded Math helper\033[0m"
 		shift ;;
 		phy)
 			echo -e "\033[38;5;9;1m Looking into the fabric of the Universe...\033[0m"
 			mkdir -p lib
 			git clone https://github.com/FlavorlessQuark/Physics_Tools tmp
-			mv tmp/*.h ../incl
-			mv tmp/*.a ../lib
+			cp tmp/*.h ../incl
+			cp tmp/*.c ../srcs
+			cp tmp/*.a ../lib
+			rm -rf tmp
 			echo -e "\033[38;5;46;1m Loaded Physics helper\033[0m"
 			shift ;;
 
 		done)
 			echo -e "\033[38;5;46;1m Teleporting away...\033[0m"
-			rm -r ../Quantum_Vault
+			rm -rf ../Quantum_Vault
+			rm vault.sh
 			break ;;
 		*)
 			break ;;
