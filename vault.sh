6e76ae16b9248f60fd5f5c16749a1e2029df6d19
Jjosephi <31075794+FlavorlessQuark@users.noreply.github.com>
2020-08-26 02:41:53 -0700

    Update vault.sh

--- vault.sh
+++ vault.sh
@@ -11,10 +11,10 @@ while : ; do
 			mkdir -p srcs incl
 			touch srcs/main.c
 			mv Quantum_Vault/Makefile .
-			echo -e "\033[38;5;46;1m Initialized ...\033[0m"
+			echo -e "\033[38;5;46;1m Initialized\033[0m"
 			shift ;;
 		sdl)
-			echo -e "\033[38;5;9;1m Opening wormhole to SDL universe...\033[0m"
+			echo -e "\033[38;5;9;1m Opening a wormhole to the SDL universe...\033[0m"
 			mkdir -p lib
 			git clone https://github.com/FlavorlessQuark/SDL_Tools tmp
 			cp tmp/*.h incl/
@@ -28,9 +28,9 @@ while : ; do
 			echo -e "\033[38;5;9;1m Summoning a portal to the Math dimension...\033[0m"
 			mkdir -p lib
 			git clone https://github.com/FlavorlessQuark/Math_Tools tmp
-			cp tmp/*.h ../incl
-			cp tmp/*.c ../srcs
-			cp tmp/*.a ../lib
+			cp tmp/*.h incl/
+			cp tmp/*.c srcs/
+			cp tmp/*.a lib/
 			rm -rf tmp
 			echo -e "\033[38;5;46;1m Loaded Math helper\033[0m"
 		shift ;;
@@ -38,9 +38,9 @@ while : ; do
 			echo -e "\033[38;5;9;1m Looking into the fabric of the Universe...\033[0m"
 			mkdir -p lib
 			git clone https://github.com/FlavorlessQuark/Physics_Tools tmp
-			cp tmp/*.h ../incl
-			cp tmp/*.c ../srcs
-			cp tmp/*.a ../lib
+			cp tmp/*.h incl/
+			cp tmp/*.c srcs/
+			cp tmp/*.a lib/
 			rm -rf tmp
 			echo -e "\033[38;5;46;1m Loaded Physics helper\033[0m"
 			shift ;;
